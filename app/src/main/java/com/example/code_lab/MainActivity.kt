e1fc782b53241d5f4a1be9e49b655ae6e51e5884
ZzCry <dariorata@gmail.com>
2023-10-25 20:37:12 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/code_lab/MainActivity.kt
+++ app/src/main/java/com/example/code_lab/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.example.code_lab
 
+import android.content.ClipData.Item
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,20 +30,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.code_lab.ui.theme.BasicsCodelabTheme
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import com.example.code_lab.ui.theme.CodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,7 +51,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
 
     Surface(modifier, color = MaterialTheme.colorScheme.background) {
         if (shouldShowOnboarding) {
@@ -77,42 +72,43 @@ fun OnboardingScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Welcome to the Basics Codelab!")
+        Text("Bienvenido al gestor de tareas")
         Button(
             modifier = Modifier.padding(vertical = 24.dp),
             onClick = onContinueClicked
         ) {
-            Text("Continue")
+            Text("Continuar")
         }
     }
 }
-
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = List(1000) { "$it" }
+    tareas: List<Pair<String, Boolean>> = generateTareas()
 ) {
-    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name)
+    LazyColumn(
+        modifier = modifier.padding(vertical = 4.dp)
+    ) {
+        items(tareas) { (nombreTarea, completada) ->
+            Greeting(nombreTarea, completada)
         }
     }
 }
 
 @Composable
-private fun Greeting(name: String) {
+private fun Greeting(nombreTarea: String, completada: Boolean) {
     Card(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.primary
         ),
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        CardContent(name)
+        CardContent(nombreTarea, completada)
     }
 }
 
 @Composable
-private fun CardContent(name: String) {
+private fun CardContent(nombreTarea: String, completada: Boolean) {
     var expanded by remember { mutableStateOf(false) }
 
     Row(
@@ -130,31 +126,93 @@ private fun CardContent(name: String) {
                 .weight(1f)
                 .padding(12.dp)
         ) {
-            Text(text = "Hello, ")
             Text(
-                text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                text = if (completada) "Tarea completada: " else "Tarea pendiente: ",
+                style = MaterialTheme.typography.headlineMedium.copy(
                     fontWeight = FontWeight.ExtraBold
                 )
             )
+
+            Text(
+                text = nombreTarea,
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold,
+                    textDecoration = if (completada) TextDecoration.LineThrough else TextDecoration.None
+                )
+            )
+
             if (expanded) {
                 Text(
-                    text = ("Composem ipsum color sit lazy, " +
-                            "padding theme elit, sed do bouncy. ").repeat(4),
+                    text = "Descripción de la tarea...",
                 )
             }
         }
-        IconButton(onClick = { expanded = !expanded }) {
-            Icon(
-                imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
-                contentDescription = if (expanded) {
-                    stringResource(R.string.show_less)
-                } else {
-                    stringResource(R.string.show_more)
-                }
+    }
+}
+
+@Composable
+private fun generateTareas(): List<Pair<String, Boolean>> {
+    return listOf(
+        "Lavar platos" to true,
+        "Lavar coche" to false,
+        "Sacar al perro" to true,
+        "Ir al gimnasio" to false,
+        "Hacer deberes" to true,
+        "Ir a clase" to false,
+    )
+}
+
+@Composable
+private fun CardContent(nombreTarea: String, completada: Boolean, onCompletadaChanged: (Boolean) -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
             )
+    ) {
+        Checkbox(
+            checked = completada,
+            onCheckedChange = { isChecked ->
+                onCompletadaChanged(isChecked)
+            },
+            modifier = Modifier.padding(end = 12.dp)
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(
+                text = if (completada) "Tarea completada: " else "Tarea pendiente: ",
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+
+            Text(
+                text = nombreTarea,
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold,
+                    textDecoration = if (completada) TextDecoration.LineThrough else TextDecoration.None
+                )
+            )
+
+            if (expanded) {
+                Text(
+                    text = "Descripción de la tarea...",
+                )
+            }
         }
     }
 }
+
+
 
 @Preview(
     showBackground = true,
